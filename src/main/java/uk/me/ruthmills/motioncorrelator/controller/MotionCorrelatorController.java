3df5381074710261a45fa5bfe8dbeb2d43ca799b
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-20 18:19:13 +0000

    Decode form body.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/MotionCorrelatorController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/MotionCorrelatorController.java
@@ -3,6 +3,7 @@ package uk.me.ruthmills.motioncorrelator.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class MotionCorrelatorController {
 
 	private final Logger logger = LoggerFactory.getLogger(MotionCorrelatorController.class);
 
-	@PostMapping(value = "/vectorLine/{camera}")
+	@PostMapping(path = "/vectorLine/{camera}", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
 	@ResponseStatus(value = HttpStatus.OK)
 	public void vector(@PathVariable String camera, @RequestBody String vectorLine) {
 		logger.info(camera + " : " + vectorLine);
