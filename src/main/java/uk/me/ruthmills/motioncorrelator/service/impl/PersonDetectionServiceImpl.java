08ec740f8ecfd312e1aea89391a4d1512e9b9a54
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-20 22:03:26 +0000

    Add Person Detection Service initial version.

---
+++ src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/PersonDetectionServiceImpl.java
@@ -0,0 +1,31 @@
+package uk.me.ruthmills.motioncorrelator.service.impl;
+
+import javax.annotation.PostConstruct;
+
+import org.opencv.objdetect.CascadeClassifier;
+import org.springframework.stereotype.Service;
+
+import uk.me.ruthmills.motioncorrelator.service.PersonDetectionService;
+
+@Service
+public class PersonDetectionServiceImpl implements PersonDetectionService {
+
+	private CascadeClassifier frontalFaceClassifier;
+	private CascadeClassifier profileFaceClassifier;
+	private CascadeClassifier upperBodyClassifier;
+	private CascadeClassifier lowerBodyClassifier;
+	private CascadeClassifier fullBodyClassifier;
+
+	@PostConstruct
+	public void initialise() {
+		frontalFaceClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+		profileFaceClassifier = new CascadeClassifier("haarcascade_profileface.xml");
+		upperBodyClassifier = new CascadeClassifier("haarcascade_upperbody.xml");
+		lowerBodyClassifier = new CascadeClassifier("haarcascade_lowerbody.xml");
+		fullBodyClassifier = new CascadeClassifier("haarcascade_fullbody.xml");
+	}
+
+	@Override
+	public void detectPerson(String camera) {
+	}
+}
