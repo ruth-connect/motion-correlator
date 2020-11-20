681aaa33478d54fddb46af934dfda5f00d8477a6
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-20 18:09:54 +0000

    Tweak to send 200 status code.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/MotionCorrelatorController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/MotionCorrelatorController.java
@@ -2,11 +2,13 @@ package uk.me.ruthmills.motioncorrelator.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/")
@@ -14,7 +16,8 @@ public class MotionCorrelatorController {
 
 	private final Logger logger = LoggerFactory.getLogger(MotionCorrelatorController.class);
 
-	@RequestMapping(value = "/vectorLine/{camera}", method = RequestMethod.POST)
+	@PostMapping(value = "/vectorLine/{camera}")
+	@ResponseStatus(value = HttpStatus.OK)
 	public void vector(@PathVariable String camera, @RequestBody String vectorLine) {
 		logger.debug(camera + " : " + vectorLine);
 	}
