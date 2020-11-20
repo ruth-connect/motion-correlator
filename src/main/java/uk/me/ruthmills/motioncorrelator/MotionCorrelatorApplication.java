49ff10ccb3a64ee9b18db9291dc68a4e41e2c06d
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-20 17:45:32 +0000

    Add Spring application.

---
+++ src/main/java/uk/me/ruthmills/motioncorrelator/MotionCorrelatorApplication.java
@@ -0,0 +1,12 @@
+package uk.me.ruthmills.motioncorrelator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionCorrelatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionCorrelatorApplication.class, args);
+	}
+}
