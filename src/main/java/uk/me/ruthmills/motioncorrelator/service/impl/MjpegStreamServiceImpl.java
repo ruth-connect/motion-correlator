2e7ce49f39594995a27d096d4309a9654b2fe6d3
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-21 21:44:02 +0000

    Try adding a viewer.

--- src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/MjpegStreamServiceImpl.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/MjpegStreamServiceImpl.java
@@ -8,17 +8,21 @@ import org.springframework.stereotype.Service;
 
 import com.bitplan.mjpegstreamer.MJpegReaderRunner1;
 
+import uk.me.ruthmills.motioncorrelator.mjpeg.MjpegRenderer;
 import uk.me.ruthmills.motioncorrelator.service.MjpegStreamService;
 
 @Service
 public class MjpegStreamServiceImpl implements MjpegStreamService {
 
+	MjpegRenderer renderer;
 	MJpegReaderRunner1 mjpegReader;
 
 	@PostConstruct
 	public void initialise() throws IOException {
+		renderer = new MjpegRenderer();
 		mjpegReader = new MJpegReaderRunner1();
 		mjpegReader.init("http://hal9000/mjpeg_stream.php", null, null);
+		mjpegReader.setViewer(renderer);
 		mjpegReader.run();
 	}
 }
