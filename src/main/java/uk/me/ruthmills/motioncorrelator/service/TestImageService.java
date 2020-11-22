5526e6ba26aed18fd73f06b570763f11445ee12f
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-22 13:17:54 +0000

    Get rid of spurious annotation.

--- src/main/java/uk/me/ruthmills/motioncorrelator/service/TestImageService.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/service/TestImageService.java
@@ -1,10 +1,7 @@
 package uk.me.ruthmills.motioncorrelator.service;
 
-import org.springframework.stereotype.Service;
-
 import uk.me.ruthmills.motioncorrelator.model.image.Image;
 
-@Service
 public interface TestImageService {
 
 	public void setImage(Image image);
