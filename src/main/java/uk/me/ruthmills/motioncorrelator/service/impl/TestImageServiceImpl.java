0cbc65e99175dd9fe67593fb4e0e0330711bfc62
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-22 13:18:59 +0000

    Sort out the test image service.

--- src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/TestImageServiceImpl.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/TestImageServiceImpl.java
@@ -3,15 +3,18 @@ package uk.me.ruthmills.motioncorrelator.service.impl;
 import org.springframework.stereotype.Service;
 
 import uk.me.ruthmills.motioncorrelator.model.image.Image;
+import uk.me.ruthmills.motioncorrelator.service.TestImageService;
 
 @Service
-public class TestImageServiceImpl {
+public class TestImageServiceImpl implements TestImageService {
 	private Image image;
 
+	@Override
 	public void setImage(Image image) {
 		this.image = image;
 	}
 
+	@Override
 	public Image getImage() {
 		if (image == null) {
 			throw new IllegalStateException("Image is null");
@@ -19,6 +22,7 @@ public class TestImageServiceImpl {
 		return image;
 	}
 
+	@Override
 	public boolean hasImage() {
 		return image != null;
 	}
