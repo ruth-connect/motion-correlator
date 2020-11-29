35ecfb10a04ccbcd315f5eae66323619a8c2ca7f
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-29 16:51:53 +0000

    Draw interpolation flag.

--- src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/ImageStampingServiceImpl.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/service/impl/ImageStampingServiceImpl.java
@@ -171,6 +171,9 @@ public class ImageStampingServiceImpl implements ImageStampingService {
 				drawText(graphics2D, "Mag: " + frameVector.getMagnitude(), 530, 410, Color.MAGENTA);
 				drawText(graphics2D, "Cnt: " + frameVector.getCount(), 530, 450, Color.MAGENTA);
 			}
+			if (vectorMotionDetection.isInterpolated()) {
+				drawText(graphics2D, "[Int]", 10, 370, Color.MAGENTA);
+			}
 		}
 	}
 
