ed9191b6cd8015380f22cefdf653759c98fe7b5b
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-25 10:22:09 +0000

    Try stream again.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/StreamController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/StreamController.java
@@ -1,8 +1,9 @@
 package uk.me.ruthmills.motioncorrelator.controller;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,26 +20,29 @@ public class StreamController {
 	private FrameService frameService;
 
 	@GetMapping(value = "/{camera}", produces = "multipart/x-mixed-replace; boundary=--BoundaryString")
-	public ResponseEntity<StreamingResponseBody> stream(@PathVariable String camera) {
-		StreamingResponseBody stream = out -> {
-			long oldSequence = 0;
-			while (true) {
-				try {
-					long currentSequence = frameService.getLatestFrame(camera).getSequence();
-					if (currentSequence != oldSequence) {
-						byte[] image = frameService.getLatestFrame(camera).getImage().getBytes();
-						out.write(("--BoundaryString\r\n" + "Content-type: image/jpeg\r\n" + "Content-Length: "
-								+ image.length + "\r\n\r\n").getBytes());
-						out.write(image);
-						out.write("\r\n\r\n".getBytes());
-						out.flush();
+	public StreamingResponseBody stream(@PathVariable String camera) {
+		return new StreamingResponseBody() {
+
+			@Override
+			public void writeTo(OutputStream out) throws IOException {
+				long oldSequence = 0;
+				while (true) {
+					try {
+						long currentSequence = frameService.getLatestFrame(camera).getSequence();
+						if (currentSequence != oldSequence) {
+							byte[] image = frameService.getLatestFrame(camera).getImage().getBytes();
+							out.write(("--BoundaryString\r\n" + "Content-type: image/jpeg\r\n" + "Content-Length: "
+									+ image.length + "\r\n\r\n").getBytes());
+							out.write(image);
+							out.write("\r\n\r\n".getBytes());
+							out.flush();
+						}
+						oldSequence = currentSequence;
+						Thread.sleep(20);
+					} catch (InterruptedException ex) {
 					}
-					oldSequence = currentSequence;
-					Thread.sleep(20);
-				} catch (InterruptedException ex) {
 				}
 			}
 		};
-		return new ResponseEntity<>(stream, HttpStatus.OK);
 	}
 }
