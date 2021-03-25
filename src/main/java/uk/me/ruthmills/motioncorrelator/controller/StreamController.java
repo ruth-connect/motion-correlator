f06cbef0454130b94d90d4f10f71b15feca3903f
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-25 09:46:24 +0000

    Initial test of stream controller.

---
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/StreamController.java
@@ -0,0 +1,41 @@
+package uk.me.ruthmills.motioncorrelator.controller;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+
+import uk.me.ruthmills.motioncorrelator.service.FrameService;
+
+@Controller
+@RequestMapping("stream")
+public class StreamController {
+
+	@Autowired
+	private FrameService frameService;
+
+	private ExecutorService executor = Executors.newCachedThreadPool();
+
+	@GetMapping(value = "/{camera}")
+	public ResponseEntity<ResponseBodyEmitter> stream(@PathVariable String camera) {
+		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+		executor.execute(() -> {
+			try {
+				byte[] image = frameService.getLatestFrame(camera).getImage().getBytes();
+				emitter.send(image, MediaType.IMAGE_JPEG);
+				emitter.complete();
+			} catch (Exception ex) {
+				emitter.completeWithError(ex);
+			}
+		});
+		return new ResponseEntity<>(emitter, HttpStatus.OK);
+	}
+}
