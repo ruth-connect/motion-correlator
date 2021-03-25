6cfd5e73073910df8dc91ee25839d68052f663e9
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-25 10:14:27 +0000

    Try again to implement MJPEG.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/StreamController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/StreamController.java
@@ -1,17 +1,13 @@
 package uk.me.ruthmills.motioncorrelator.controller;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import uk.me.ruthmills.motioncorrelator.service.FrameService;
 
@@ -22,22 +18,25 @@ public class StreamController {
 	@Autowired
 	private FrameService frameService;
 
-	private ExecutorService executor = Executors.newCachedThreadPool();
-
-	@GetMapping(value = "/{camera}")
-	public ResponseEntity<ResponseBodyEmitter> stream(@PathVariable String camera) {
-		ResponseBodyEmitter emitter = new ResponseBodyEmitter();
-		executor.execute(() -> {
+	@GetMapping(value = "/{camera}", produces = "multipart/x-mixed-replace; boundary=--BoundaryString")
+	public ResponseEntity<StreamingResponseBody> stream(@PathVariable String camera) {
+		StreamingResponseBody stream = out -> {
+			long oldSequence = 0;
 			try {
-				while (true) {
+				long currentSequence = frameService.getLatestFrame(camera).getSequence();
+				if (currentSequence != oldSequence) {
 					byte[] image = frameService.getLatestFrame(camera).getImage().getBytes();
-					emitter.send(image, MediaType.IMAGE_JPEG);
-					Thread.sleep(200);
+					out.write(("--BoundaryString\r\n" + "Content-type: image/jpeg\r\n" + "Content-Length: "
+							+ image.length + "\r\n\r\n").getBytes());
+					out.write(image);
+					out.write("\r\n\r\n".getBytes());
+					out.flush();
 				}
-			} catch (Exception ex) {
-				emitter.completeWithError(ex);
+				oldSequence = currentSequence;
+				Thread.sleep(20);
+			} catch (InterruptedException ex) {
 			}
-		});
-		return new ResponseEntity<>(emitter, HttpStatus.OK);
+		};
+		return new ResponseEntity<>(stream, HttpStatus.OK);
 	}
 }
