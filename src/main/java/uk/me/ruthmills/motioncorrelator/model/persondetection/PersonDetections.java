3e1663767f057d414a1be2ecb2a3d7c648afed8a
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-27 20:27:30 +0000

    Make serializable.

--- src/main/java/uk/me/ruthmills/motioncorrelator/model/persondetection/PersonDetections.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/model/persondetection/PersonDetections.java
@@ -6,10 +6,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class PersonDetections {
 
 	private List<PersonDetection> personDetections;
 	private long detectionTimeMilliseconds;
+
+	public PersonDetections() {
+	}
 
 	public PersonDetections(List<PersonDetection> personDetections, long detectionTimeMilliseconds) {
 		this.personDetections = personDetections;
@@ -24,6 +29,7 @@ public class PersonDetections {
 		return personDetections;
 	}
 
+	@JsonIgnore
 	public PersonDetection getStrongestPersonDetection() {
 		if (personDetections == null || personDetections.size() == 0) {
 			return null;
@@ -36,6 +42,7 @@ public class PersonDetections {
 		return detectionTimeMilliseconds;
 	}
 
+	@JsonIgnore
 	public String getDetectionsFilename(long sequence, LocalDateTime timestamp) {
 		String detections = personDetections.size() > 0 ? "-" + personDetections.size() + "-"
 				+ new BigDecimal(personDetections.get(0).getWeight()).setScale(3, RoundingMode.HALF_UP) : "";
