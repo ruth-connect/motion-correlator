7c8123f3ea06288985e61e8e4cd9cf0ad3d65f53
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-27 09:34:00 +0000

    Sort alphabetically by location description.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/HomeController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/HomeController.java
@@ -1,6 +1,8 @@
 package uk.me.ruthmills.motioncorrelator.controller;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import uk.me.ruthmills.motioncorrelator.model.Camera;
 import uk.me.ruthmills.motioncorrelator.service.CameraService;
 
 @Controller
@@ -19,7 +22,8 @@ public class HomeController {
 
 	@GetMapping(path = "/")
 	public String showHomePage(Model model) throws IOException {
-		model.addAttribute("cameras", cameraService.getCameras());
+		model.addAttribute("cameras", cameraService.getCameras().stream()
+				.sorted(Comparator.comparing(Camera::getLocationDescription)).collect(Collectors.toList()));
 		return "index";
 	}
 }
