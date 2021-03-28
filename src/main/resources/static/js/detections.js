238e8cd5c49c4e542f1de6542fa5b906a214722a
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-28 23:07:30 +0100

    Truncate the weights to 3 decimal places.

--- src/main/resources/static/js/detections.js
+++ src/main/resources/static/js/detections.js
@@ -70,7 +70,7 @@ function displayNewDetections(detections) {
 					"<td>" + (detection.vectorMotionDetection ? detection.vectorMotionDetection.frameVector.count : "") + "</td>" +
 					"<td>" + (detection.vectorMotionDetection && detection.vectorMotionDetection.interpolated ? "Y" : "") + "</td>" +
 					"<td>" + (detection.personDetections && detection.personDetections.personDetections && detection.personDetections.personDetections.length > 0 ?
-						new String(detection.personDetections.personDetections[0].weight).padEnd(5, "0") : "") + "</td>" +
+						detection.personDetections.personDetections[0].weight.toFixed(3).padEnd(5, "0") : "") + "</td>" +
 					"<td>" + (detection.personDetections ? detection.personDetections.detectionTimeMilliseconds + "ms" : "") + "</td>" +
 				"</tr>";
 			
