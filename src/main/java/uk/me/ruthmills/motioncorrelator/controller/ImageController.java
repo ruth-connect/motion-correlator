ec3026847a22a445047c3f1b8114ea92057eada0
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-28 13:56:01 +0100

    Add content type.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/ImageController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/ImageController.java
@@ -3,6 +3,7 @@ package uk.me.ruthmills.motioncorrelator.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ public class ImageController {
 	@Autowired
 	private ImageFileService imageFileService;
 
-	@GetMapping(path = "/{camera}/{year}/{month}/{day}/{hour}/{filename}")
+	@GetMapping(path = "/{camera}/{year}/{month}/{day}/{hour}/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] getImage(@PathVariable String camera, @PathVariable String year, @PathVariable String month,
 			@PathVariable String day, @PathVariable String hour, @PathVariable String filename) throws IOException {
 		return imageFileService.readImage(camera, year, month, day, hour, filename);
