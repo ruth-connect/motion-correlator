9b0ac066e2eb361b0476ed6d99ba488cd29627b9
ruth <ruth@DESKTOP-HOVL7PT>
2021-03-30 10:56:14 +0100

    Show placeholder image if error.

--- src/main/java/uk/me/ruthmills/motioncorrelator/controller/StampedImageController.java
+++ src/main/java/uk/me/ruthmills/motioncorrelator/controller/StampedImageController.java
@@ -1,7 +1,12 @@
 package uk.me.ruthmills.motioncorrelator.controller;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -28,15 +33,24 @@ public class StampedImageController {
 	@Autowired
 	private ImageStampingService imageStampingService;
 
+	private static final Logger logger = LoggerFactory.getLogger(StampedImageController.class);
+
 	@GetMapping(path = "/{camera}/{year}/{month}/{day}/{hour}/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
 	@ResponseBody
 	public byte[] getImage(@PathVariable String camera, @PathVariable String year, @PathVariable String month,
 			@PathVariable String day, @PathVariable String hour, @PathVariable String filename) throws IOException {
-		String[] filenameParts = filename.substring(0, filename.lastIndexOf(".")).split("-");
-		String timestamp = filenameParts[0] + "-" + filenameParts[1] + "-" + filenameParts[2];
-		String sequence = filenameParts[3];
-		Detection detection = detectionFileService.readDetection(camera, year, month, day, hour, timestamp, sequence);
-		byte[] jpeg = imageFileService.readImage(camera, year, month, day, hour, filename);
-		return imageStampingService.stampImage(detection, jpeg);
+		try {
+			String[] filenameParts = filename.substring(0, filename.lastIndexOf(".")).split("-");
+			String timestamp = filenameParts[0] + "-" + filenameParts[1] + "-" + filenameParts[2];
+			String sequence = filenameParts[3];
+			Detection detection = detectionFileService.readDetection(camera, year, month, day, hour, timestamp,
+					sequence);
+			byte[] jpeg = imageFileService.readImage(camera, year, month, day, hour, filename);
+			return imageStampingService.stampImage(detection, jpeg);
+		} catch (Exception ex) {
+			logger.error("Failed to read stamped image", ex);
+		}
+		Path path = FileSystems.getDefault().getPath("src/main/resources", "image-not-available.jpg");
+		return Files.readAllBytes(path);
 	}
 }
